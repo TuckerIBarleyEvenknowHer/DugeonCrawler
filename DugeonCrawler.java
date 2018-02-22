8fe559f2d1a42b8b0ad389b13b05ad696cacedeb
Dzion Tucker <36012047+TuckerIBarleyEvenknowHer@users.noreply.github.com>
2018-02-22 12:52:18 -0500

    Add files via upload

--- DugeonCrawler.java
+++ DugeonCrawler.java
@@ -5,6 +5,9 @@
  */
 package dugeoncrawler;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author TuckerDzion
@@ -15,7 +18,27 @@ public class DugeonCrawler {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Move by typing left or right");
+        /*starting instruction
+        
+        */
+       System.out.println("Get to the end of the Dungeon whcih way do you want to move ");
+        /*the first movmentment 
+       
+       */
+       
+       
+   }
+    private class Dungeons {
+
+        String[] Dugeon = new String[3];
+    /** Creates a new instance of Array */
+    public Dungeons() {
+        Dugeon[1]= "hero";
     }
-    
+}
+    public static void Hero(String[] args){
+        
+    }
 }
